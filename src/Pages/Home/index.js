2c726591bb8ed434f6ad8451eaa727f5a2f40f13
prathyusha <naraharisreeja123@gmail.com>
2024-02-01 13:37:51 -0600

    Update login Form

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Card } from "antd";
-import "./index.css";
+import "./home.css";
 import GoogleLoginComp from "../../Components/GoogleLoginComp";
+import { Input, Tooltip, Button } from "antd";
+import {
+  InfoCircleOutlined,
+  UserOutlined,
+  EyeInvisibleOutlined,
+  EyeTwoTone,
+} from "@ant-design/icons";
 
 const Home = () => {
   return (
@@ -9,9 +16,28 @@ const Home = () => {
       <div className="login-card-container">
         <Card
           className="login-card"
-          title="Please login with your Gmail."
+          title="Please login"
           headStyle={{ color: "#ffffff" }}>
-          <GoogleLoginComp />
+          <Input
+            placeholder="Enter your username"
+            className="username"
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            suffix={
+              <Tooltip title="Extra information">
+                <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
+              </Tooltip>
+            }
+          />
+          <Input.Password
+            placeholder="Enter password"
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+          />
+          <Button className="login-button" type="text">
+            Login
+          </Button>
+          {/* <GoogleLoginComp /> */}
         </Card>
       </div>
     </div>
